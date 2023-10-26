bb59bc95666eb1cdaa122b275daaec8e8753c084
Tehfalk <didrik.thorso.dt@gmail.com>
2023-10-26 12:35:26 +0200

    add output element
    add output element to show list when button pressed
    
    #2

---
+++ script.js
@@ -0,0 +1,12 @@
+function addTask() {
+    var taskInput = document.getElementById("task");
+    var taskList = document.getElementById("taskList");
+    var taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        var taskItem = document.createElement("li");
+        taskItem.textContent = taskText;
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+    }
+}
