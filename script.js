371ccfc7e8005a53ef81cbe3d30db7df65f1d56e
Tehfalk <didrik.thorso.dt@gmail.com>
2023-10-27 11:22:42 +0200

    Add fullført and checkbox
    add task fullført and checkbox for task
    
    #4

--- script.js
+++ script.js
@@ -5,8 +5,45 @@ function addTask() {
 
     if (taskText !== "") {
         var taskItem = document.createElement("li");
-        taskItem.textContent = taskText;
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        taskItem.appendChild(checkbox);
+        taskItem.appendChild(document.createTextNode(taskText));
         taskList.appendChild(taskItem);
         taskInput.value = "";
     }
+}
+
+function addTask() {
+    var taskInput = document.getElementById("task");
+    var taskList = document.getElementById("taskList");
+    var taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        var taskItem = document.createElement("li");
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        taskItem.appendChild(checkbox);
+        taskItem.appendChild(document.createTextNode(taskText));
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+
+        checkbox.addEventListener("change", updateTaskCount);
+    }
+}
+
+function updateTaskCount() {
+    var checkboxes = document.querySelectorAll("input[type=checkbox]");
+    var completedCount = 0;
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            completedCount++;
+        }
+    }
+
+    var totalCount = checkboxes.length;
+
+    var taskCount = document.getElementById("taskCount");
+    taskCount.textContent = `${completedCount}/${totalCount} fullført`;
 }
